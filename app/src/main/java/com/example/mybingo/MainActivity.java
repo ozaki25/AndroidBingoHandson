4fede0ec456510525cfc6f31ff2ac0a31952c50c
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-26 13:47:27 +0900

    ボタンを押してランダムな値を出すまで

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mybingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView historyView;
+    private TextView numberView;
+    private View button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        historyView = findViewById(R.id.historyView);
+        numberView = findViewById(R.id.numberView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberView.setText("" + System.currentTimeMillis());
+            }
+        });
     }
 }
