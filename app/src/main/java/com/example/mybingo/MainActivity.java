88ad101f48b84804b3fc87a6d7190abaf142a583
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-26 14:04:59 +0900

    重複した数が出ないようにして全部出尽くしたらボタンを非活性化

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -16,12 +16,18 @@ public class MainActivity extends AppCompatActivity {
     private View button;
 
     private final Random random = new Random();
+    private final List<Integer> pickNumbers = new ArrayList<>();
     private final List<Integer> histories = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pickNumbers.clear();
+        for (int i = 1; i <= 75; i++) {
+            pickNumbers.add(i);
+        }
 
         historyView = findViewById(R.id.historyView);
         numberView = findViewById(R.id.numberView);
@@ -30,11 +36,16 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final int nextNumber = random.nextInt(75) + 1;
+                final int index = random.nextInt(pickNumbers.size());
+                final int nextNumber = pickNumbers.remove(index);
                 numberView.setText("" + nextNumber);
 
                 histories.add(nextNumber);
                 historyView.setText(createHistoryText());
+
+                if (pickNumbers.isEmpty()) {
+                    button.setEnabled(false);
+                }
             }
         });
     }
