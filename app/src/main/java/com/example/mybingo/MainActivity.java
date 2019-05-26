b7c8241d665169d5dacb77c30bdbfdf17dfe847d
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-26 13:48:22 +0900

    1~75のどれかが出るようにする

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private TextView historyView;
     private TextView numberView;
     private View button;
+
+    private final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberView.setText("" + System.currentTimeMillis());
+                final int nextNumber = random.nextInt(75) + 1;
+                numberView.setText("" + nextNumber);
             }
         });
     }
